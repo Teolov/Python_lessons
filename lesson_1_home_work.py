77881d46aad1d098f47d7b47eaec596d497ebdba
Teolov <mikhaylov.mikhaylof@gmail.com>
2024-09-23 20:17:11 +0300

    lesson_2

---
+++ lesson_1_home_work.py
@@ -0,0 +1,41 @@
+# # task_1
+
+my_first = 'Моя первая переменная!'
+print(my_first)
+
+
+
+## task_2
+
+name = 'Иван'
+my_color = 'Зелёный'
+sport = 'Бег'
+
+print('Меня зовут:')
+print(name)
+print('Мой любимый цвет:')
+print(my_color)
+print('Мой любимый вид спорта:')
+print(sport)
+
+## или так
+# print('Меня зовут: ' + name)
+# print('Мой любимый цвет: '  + my_color)
+# print('Мой любимый вид спорта: ' + sport)
+
+
+
+## task_3
+num_1 = 5684721
+num_2 = 1421698
+print(str(num_1) + str(num_2))
+
+## or
+num_3 = str(51251)
+num_4 = str(12167)
+print(num_3 + num_4)
+
+##or
+text_1 = 'Hello '
+text_2 = 'World!'
+print(text_1 + text_2)
---
+++ lesson_1_number.py
@@ -0,0 +1,43 @@
+# num = 1
+# num_2 = num + 4
+# print(num, num_2)
+
+# num = 1
+# num = num + 4
+# print(num)
+
+# num = 1
+# num = ( num + 4 ) /2
+# print(num)
+
+# num_int = 26
+# num_float = 3.6
+# text_str = '4ffff'
+# bool_num = True
+
+# red_cat = 5
+# redCatDog = 5
+
+# print(2*5)
+
+# num_1 = 2 + 2
+# num_2 = 2 - 2
+# num_3 = 2 * 2
+# num_4 = 2 ** 2
+
+# num_5 = 10 / 3
+# num_6 = 10 // 3
+# num_7 = 10 % 3
+# print(num_5)
+# print(num_6)
+# print(num_7)
+
+# text_1 = 'text 1'
+# text_2 = 'text 2'
+
+# print('hel' + 'lo')
+# print('hel_' * 3)
+
+text_a1 = '6'
+print(text_a1 * 4)
+print(type(text_f1))
