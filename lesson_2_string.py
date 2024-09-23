ca07267fdf75a447502595eb863840409f2c1a5d
Teolov <mikhaylov.mikhaylof@gmail.com>
2024-09-23 20:17:51 +0300

    lesson_2

---
+++ lesson_2_string.py
@@ -0,0 +1,38 @@
+# str_num = input('введите строку__')
+# str_num = int(str_num)
+# print(type(str_num))
+# print(str_num *2)5
+
+
+# #спецсимволы
+# print('q\nw\ne\nr\nt\ny')
+# print('\\')
+
+
+# str_var_1 = 'qwerty'
+# print(str_var_1[0])
+# str_2 = str_var_1[1] + str_var_1[4]
+# print(str_2)
+# print(str_2 + str_var_1[1] + str_var_1[4])
+
+
+# print(len(str_var_1))
+
+# print(str_var_1.find('ert'))
+# print(str_var_1.find('ert') + len('ert') - 1)
+
+# str_var_3 = '1251251ert_12515ert6236236'
+# print(str_var_3.find('ert', 1))
+# print(str_var_3.find('ert', 10))
+
+# str_var_4 = '1251251'
+# print(str_var_4.isdigit())
+
+# print(str_var_4.count('1'))
+
+
+num = 45
+# str_var_5 = 'qwerty' + str(45)
+str_var_5 = f'qwerty{num}  + 45'
+print(str_var_5)
+
