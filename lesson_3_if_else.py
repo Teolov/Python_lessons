0e6b5a4e0e60ab9097ab12d103f17a8f253612ca
Teolov <mikhaylov.mikhaylof@gmail.com>
2024-09-30 19:04:38 +0300

    30_09

---
+++ lesson_3_if_else.py
@@ -0,0 +1,48 @@
+# if(True):
+#     print('ok')
+
+# num = 1
+# print(num==1)
+# print(num!=1)
+# print(num<1)
+# print(num>1)
+# print(num>=1)
+# print(num<=1)
+
+# num = 1
+# if(not False):
+#     print('ok')
+# if(not(num == 2)):
+#     print('ok')
+
+# num = 1
+# if(True and False):
+#     print('ok1')
+# if(True or False):
+#     print('ok2')
+
+# num_in = int(input('Введите число: '))
+# if num_in > 10 and num_in < 50:
+#     print(num_in)
+# else:
+#     print('go away')
+
+# num_in_3 = int(input('Введите число: '))
+# if(num_in_3 == 5):
+#     print('Ok 1')
+# elif(num_in_3 < 100):
+#     print('Ok 2')
+# elif(num_in_3 != 10):
+#     print('Ok 3')
+# else:
+#     print('Not')
+
+# str_v = 'qwerty'
+# if(str_v[0] == 'q'):
+#     print('ok')
+
+#operators prinadlejnosti
+num_q1 = 8
+print(num_q1)
+num_q1 += 8
+print(num_q1)
