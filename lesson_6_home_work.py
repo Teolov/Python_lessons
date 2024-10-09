34f2eaf94e643597a9a20276b8d1a1d3b4075589
Teolov <mikhaylov.mikhaylof@gmail.com>
2024-10-09 09:55:40 +0300

    09-10

---
+++ lesson_6_home_work.py
@@ -0,0 +1,61 @@
+# Упражнение 1
+# Создайте список из имен-строк. Затем добавьте в этот список два новых имени
+# и удалите последнее имя. Выведите финальный список на консоль.
+
+# sps_1 = ['one', 'two', 'three', 'four']
+# sps_1.append('five')
+# sps_1.append('six')
+# print(sps_1)
+# sps_1.pop()
+# print(sps_1)
+
+# Упражнение 4
+# Напишите программу, которая удаляет дубликаты из списка.
+
+from random import *
+
+# sps_2 = []
+# for i in range(10):
+#     sps_2.append(randint(1,5))
+
+# print(sps_2)
+# sps_2.sort()
+# print(sps_2)
+
+# i = 0
+# while i < len(sps_2) - 1:
+#     if sps_2[i] == sps_2[i + 1]: 
+#         sps_2.pop(i) 
+#     else:
+#         i += 1  
+# print(sps_2)
+
+# Упражнение 7
+# Напишите программу, которая удаляет из списка чисел четные числа.
+
+# sps_3 = [randint(1,50) for i in range(11)]
+# print(sps_3)
+
+# for i in range(len(sps_3) -1, -1, -1):
+#     if sps_3[i] % 2 == 0:
+#         sps_3.pop(i)
+    
+# print(sps_3)
+
+# Упражнение 8
+# Напишите функцию, которая принимает два списка и возвращает новый список, в котором каждый
+# элемент представляет сумму соответствующих элементов обоих списков.
+
+# sps_4 = [randint(1,100) for i in range(5)]
+# sps_5 = [randint(1,100) for i in range(5)]
+# sps_sum = []
+# print(sps_4)
+# print(sps_5)
+
+
+# for i in range(len(sps_4)):
+#     sps_sum.append(sps_4[i] + sps_5[i])
+        
+        
+# print(sps_sum)
+
---
+++ lesson_7_function.py
@@ -0,0 +1,38 @@
+def fun_1():
+    print('ok')
+    
+# fun_1()
+# print('hello')
+
+# num_1 = 9
+# def fun_2():
+#     print(num_1)
+# fun_2()
+
+# def fun_3():
+#     global num_3
+#     num_3 = 0
+#     print(num_3)
+# fun_3()
+# print(num_3)
+
+# def fun_5(a):
+#     print(a)
+# fun_5('pip')
+
+# def fun_6(a: int):
+#     print(a)
+# fun_6(1568)
+
+# def fun_7(a, b, s):
+#     print(a + b + s)
+# fun_7(5,5,5)
+
+# def fun_8():
+#     return 0
+# print(fun_8())
+
+def fun_9(num_a = 1):
+    return num_a ** 3
+
+print(fun_9(5))
