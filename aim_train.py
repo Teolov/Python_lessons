21b1014ca6c7f99720940197294619a383e252a4
Teolov <mikhaylov.mikhaylof@gmail.com>
2024-12-08 18:32:18 +0300

    08-12

--- aim_train.py
+++ aim_train.py
@@ -12,7 +12,6 @@ window.resizable(False, False)
 def mouse_b1(event):
     target.place(w = 50, h = 50, x=(randint(50,screen_width-100)), y=(randint(50,screen_height-100)))
     
-
 def mouse_pressed(event):
     target.config(bg = 'red')
 
@@ -22,8 +21,6 @@ def mouse_release(event):
 target = Label()
 target.config(bg = "#000000")
 target.place(w = 50, h = 50, x=(randint(50,screen_width-50)), y=(randint(50,screen_height-50)))
-
-
 
 target.bind("<Button-1>" , mouse_b1)
 target.bind("<ButtonPress>", mouse_pressed)
